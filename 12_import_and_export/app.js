bdf320efd33ee2ad162886dd8a2275b228f15aed
Tanishq Mathur <tanishqmathur975@gmail.com>
2026-01-29 20:49:39 +0530

    Import and Export

---
+++ 12_import_and_export/app.js
@@ -0,0 +1,7 @@
+export const arr = [10, 20, 30, 40]
+export const age = 20
+
+const user = "tanishq"
+
+
+export default user
---
+++ 12_import_and_export/script.js
@@ -0,0 +1,5 @@
+import username from './app.js'
+import { arr, age } from './app.js'
+
+console.log(arr)
+console.log(username, age)
